fccac247c8a0a4156deaafb737585ad4d7550a9f
madhavprapanna52 <madhavprapanna@gmail.com>
2026-01-14 10:29:42 +0530

    interface for transaction object new issue created :)

--- models/Transaction.py
+++ models/Transaction.py
@@ -1,23 +1,53 @@
 """
-    Mini transaction DB connection unit for storing user history
+    Transaction unit for Managing transaction DB
 
+    Make bill()
+   Iterate products in cart instance
+    Compute information and bill about product
+
+    Pay_bill()
+    Function for completing the payment via user
+    Add products with user id into db for final transaction
+    completes transaction
 """
-from config import *
+from DB.General_Object import *
+from models.Product import *
 
 
-def transaction_db(final_bill, user_id, db_endpoint):
-    db_endpoint = db_endpoint
-    final_dict = {
-        "customer_id" : user_id,
-        "product_id" : 0,
-        "stock" : 0
-    }
-    for elem in final_bill:
-        print(f"elemenet is {elem} with type : {type(elem)}")
-        product_id = elem[0]
-        stock = elem[1]
-        final_dict["product_id"] = product_id
-        final_dict["stock"] = stock
-        print(f"Final dictionary : {final_dict}")
-        db_endpoint.create_row(final_dict)
-        log.info("Check out transaction db for update regards the purchase")
+class Transaction(General_Object):
+    """
+    # BUG Transaction object cant use general object due to low resources requriements
+        Information : Cart instance
+        dictionary with key as product name and value as quantity
+    """
+    def __init__(self, information, transaction_endpoint, product_endpoint):
+        self.endpoint = transaction_endpoint
+        self.product_endpoint = product_endpoint
+        # Fetch information about products
+        products = list(information.keys())
+        bill_list = []  # product_name, prize , stock required
+        total_bill = 0
+        for product in products:
+            product_object = self.init_product(product)  # product object
+            product_prize = int(product_object.information["prize"])
+            stock = int(information[product])  # stock information 
+            bill_element = (product, product_prize, stock)  # bill element
+            bill_list.append(bill_element)  # bill builds up 
+            total_bill += (product_prize * stock)
+            del product_object
+        self.bill_list = bill_list  # Final bill list for transaction
+        self.total_bill = total_bill  # total bill 
+
+    def product_endpoint(self):
+        for element in self.bill_list:
+            p = self.init_product(element[0])
+            stock_change = int(element[2])
+            p.stock(stock_change)  # stock update
+
+    def init_product(self, product_name):
+        info = ("name", product_name)
+        product = Product(info, self.product_endpoint)
+        return product
+
+
+
