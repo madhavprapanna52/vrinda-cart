756c9ee6f3e353d1e4c01b3a2c39c12c391b8a3f
madhavprapanna52 <madhavprapanna@gmail.com>
2026-01-19 07:04:23 +0530

    Very simple but expandable product component

---
+++ static/js/components/Product_component.js
@@ -0,0 +1,29 @@
+/* Product component
+ 
+functions 
+  1. fetches product details and displays the product
+  2. add to cart, show details options with simple image rendering thing
+*/ 
+class ProductComponent extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open'});
+
+    // load prebuilt template
+    const template = document.getElementById('product-template').content;
+    shadow.appendChild(template.cloneNode(true));
+  }
+
+  // Page update
+  connectedCallback() {
+    // Edit template based on tags attributes
+    const name = this.getAttribute('name') || "Product";
+    const price = this.getAttribute('price') || "0.00";
+
+    this.shadowRoot.getElementById('title').textContent = name;
+    this.shadowRoot.getElementById('price').textContent = price;
+  }
+
+}
+
+customElements.define('product-card', ProductComponent);
